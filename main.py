ff7b054aebe73314b52f15f38997389458507c3b
rajrishi-06 <rajrishireddyk@gmail.com>
2025-02-21 14:17:38 +0530

    added search bar in navbar

--- main.py
+++ main.py
@@ -412,4 +412,4 @@ def submit_review(product_id):
     return redirect(url_for('view_product', product_id=product_id))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
